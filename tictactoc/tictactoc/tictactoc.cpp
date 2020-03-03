b425611a7410b9f267a3a59cadbf5111f7206fc3
Elizabeth <wonaiet@gmail.com>
2020-03-03 19:41:43 +0800

    try to debug of bool winGame, change it into wg1 wg2 34, but still bug, try exist next version

--- tictactoc/tictactoc/tictactoc.cpp
+++ tictactoc/tictactoc/tictactoc.cpp
@@ -16,7 +16,7 @@ void CheckArray(char TicTacToe[][3], int lengthOfGame)
     }
 }
 
-void checkWinOfRow(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* winner)
+void checkWinOfRow(char TicTacToe[][3], int lengthOfGame, bool* winGame1, char* winner)
 {
     // win of row
     for (int row = 0; row < lengthOfGame; row++)
@@ -25,22 +25,22 @@ void checkWinOfRow(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* w
         {
             if (TicTacToe[row][column] != TicTacToe[row][column + 1])
             {
-                *winGame = false;
+                *winGame1 = false;
                 break;
             }
             else if (TicTacToe[row][column] == '/')
             {
-                *winGame = false;
+                *winGame1 = false;
                 break;
             }
             else
             {
-                *winGame = true;
+                *winGame1 = true;
             }
 
         }
         // if wingame, cout the row number
-        if (*winGame)
+        if (*winGame1)
         {
             // check who wins
             *winner = TicTacToe[row][0];
@@ -52,7 +52,7 @@ void checkWinOfRow(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* w
 
 }
 
-void checkWinOfCloumn(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* winner)
+void checkWinOfCloumn(char TicTacToe[][3], int lengthOfGame, bool* winGame2, char* winner)
 {
     // win of column
     for (int column = 0; column < lengthOfGame; column++)
@@ -61,22 +61,22 @@ void checkWinOfCloumn(char TicTacToe[][3], int lengthOfGame, bool* winGame, char
         {
             if (TicTacToe[row][column] != TicTacToe[row + 1][column])
             {
-                *winGame = false;
+                *winGame2 = false;
                 break;
             }
             else if (TicTacToe[row][column] == '/')
             {
-                *winGame = false;
+                *winGame2 = false;
                 break;
             }
             else
             {
-                *winGame = true;
+                *winGame2 = true;
             }
 
         }
         // if wingame, cout the row number
-        if (*winGame)
+        if (*winGame2)
         {
             // check who wins
             *winner = TicTacToe[0][column];
@@ -88,7 +88,7 @@ void checkWinOfCloumn(char TicTacToe[][3], int lengthOfGame, bool* winGame, char
 
 }
 
-void checkDiagonal1(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* winner)
+void checkDiagonal1(char TicTacToe[][3], int lengthOfGame, bool* winGame3, char* winner)
 {
 
     // win of diagonal from leftup to right down
@@ -97,21 +97,21 @@ void checkDiagonal1(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* 
 
         if (TicTacToe[lenth][lenth] != TicTacToe[lenth + 1][lenth + 1])
         {
-            *winGame = false;
+            *winGame3 = false;
             break;
         }
         else if (TicTacToe[lenth][lenth] == '/')
         {
-            *winGame = false;
+            *winGame3 = false;
             break;
         }
         else
         {
-            *winGame = true;          
+            *winGame3 = true;          
         }
         // if wingame, cout the row number
     }
-    if (*winGame)
+    if (*winGame3)
     {
         // check who wins
         *winner = TicTacToe[0][0];
@@ -119,7 +119,7 @@ void checkDiagonal1(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* 
     }
 }
 
-void checkDiagonal2(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* winner)
+void checkDiagonal2(char TicTacToe[][3], int lengthOfGame, bool* winGame4, char* winner)
 {
     // win of diagonal westsorthern to northestern
     int lenth = lengthOfGame;
@@ -129,24 +129,24 @@ void checkDiagonal2(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* 
     {
         if (TicTacToe[row][column] != TicTacToe[row - 1][column + 1])
         {
-            *winGame = false;
+            *winGame4 = false;
             break;
         }
         else if (TicTacToe[row][column] == '/')
         {
-            *winGame = false;
+            *winGame4 = false;
             break;
         }
         else
         {
-            *winGame = true;
+            *winGame4 = true;
             column++;
         }
 
     }
 
     // if wingame, cout the row number
-    if (*winGame)
+    if (*winGame4)
     {
         *winner = TicTacToe[0][lenth - 1];
         cout << *winner << " is win in diagonal\n";
@@ -158,17 +158,24 @@ void checkDiagonal2(char TicTacToe[][3], int lengthOfGame, bool* winGame, char* 
 
 int main()
 {
-   char TicTacToe[3][3]=
+  
+    char TicTacToe[3][3]=
    {
-        {'/', '/', '/'}, {'/', '/', '/'}, {'/', '/', '/'}
+       {'/', '/', '/'}, {'/', '/', '/'}, {'/', '/', '/'}
+ //        {'o', 'o', 'o'}, {'/', '/', '/'}, {'/', '/', '/'}
    };
 
    // enter the row and column of your chosse
    int lengthOfGame = 3;
 
+   // initalization and 1st print the array
    CheckArray(TicTacToe, lengthOfGame);  
 
-   bool winGame = true;
+   bool winGame1 = true;
+   bool winGame2 = true;
+   bool winGame3 = true;
+   bool winGame4 = true;
+
    char winner = 0;
    int rowOfChess = 0, columnOfChess = 0;
 
@@ -194,6 +201,8 @@ int main()
            cout << player1 << " put your chess in column ";
            cin >> columnOfChess;
 
+         //  check invalid value of row and column 
+
            TicTacToe[rowOfChess - 1][columnOfChess - 1] = 'X';
         } 
         else
@@ -210,17 +219,17 @@ int main()
 
     //win check
  
-        checkWinOfRow(TicTacToe,lengthOfGame, &winGame, &winner);
-        checkWinOfCloumn(TicTacToe,lengthOfGame, &winGame, &winner);
-        checkDiagonal1(TicTacToe, lengthOfGame, &winGame, &winner);
-        checkDiagonal2(TicTacToe, lengthOfGame, &winGame, &winner);
-        if (winGame)
+        checkWinOfRow(TicTacToe,lengthOfGame, &winGame1, &winner);
+        checkWinOfCloumn(TicTacToe,lengthOfGame, &winGame2, &winner);
+        checkDiagonal1(TicTacToe, lengthOfGame, &winGame3, &winner);
+        checkDiagonal2(TicTacToe, lengthOfGame, &winGame4, &winner);
+        if (winGame1 || winGame2 || winGame3 || winGame4)
         {
             cout << "winner is " << winner;
             break;
         }
    }
-   if (!winGame)
+   if (!winGame1 && !winGame2 && !winGame3 && !winGame4)
    {
         cout << "nobadywins " << endl;
    }
